f0c87f8b891ad30204a5e7dc714d757fcc7ab1d1
adrien-thierry <adrien.thierry@live.fr>
2015-06-19 14:26:33 +0200

    Create hello.php

---
+++ file/hello.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+Author: Adrien Thierry
+Licence: GPLv2 or later
+http://seraum.com
+http://asylum.seraum.com
+http://hackmyfortress.com
+*/
+
+/* A SIMPLE HELLO WORLD FILE */
+
+echo "HELLO WORLD";
+
+?>
